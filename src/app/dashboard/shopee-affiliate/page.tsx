e4fe0570046cd35193c8a9165f6658339e51b4a5
cvsz <seaza@msn.com>
2026-05-20 13:40:08 +0700

    feat(shopee): add affiliate real database dashboard page

---
+++ src/app/dashboard/shopee-affiliate/page.tsx
@@ -0,0 +1,8 @@
+import { ShopeeAffiliateRealDbDashboard } from "@/components/shopee/ShopeeAffiliateRealDbDashboard";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function ShopeeAffiliateDashboardPage() {
+  return <ShopeeAffiliateRealDbDashboard />;
+}
